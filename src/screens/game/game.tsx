f0bc9a90cdada21739d009f98431db5a2fa988f3
binoy638 <binoy638@gmail.com>
2021-07-26 15:08:42 +0530

    done setting up

---
+++ src/screens/game/game.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import style from "./game.styles";
+
+export default function Game() {
+  return (
+    <View style={style.container}>
+      <Text>Game</Text>
+    </View>
+  );
+}
